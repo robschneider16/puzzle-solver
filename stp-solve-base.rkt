090351fe53b166787bfb4112015fb5a19ce6ad46
Wayne Iba <iba@westmont.edu>
2013-07-03 16:44:36 -0700

    split code for common and cluster/single-specific

--- stp-solve-base.rkt
+++ stp-solve-base.rkt
@@ -2,6 +2,9 @@
 
 (require srfi/25) ;; multi-dimensional arrays
 
+(provide (all-defined-out))
+
+
 (define *num-pieces* 0)
 (define *piece-types* empty)
 (define *start* empty)
@@ -229,24 +232,4 @@
     pos))
 
 (define *max-depth* 10)(set! *max-depth* 32)
-;; fringe-search: (setof position) (setof position) int -> ...
-;; perform a fringe BFS starting at the given state until depth is 0
-(define (fringe-search prev-fringe current-fringe depth)
-  (cond [(or (set-empty? current-fringe) (> depth *max-depth*)) #f]
-        [else
-         (let ((maybe-goal (goal-in-fringe? current-fringe)))
-           (cond [maybe-goal
-                  (print "found goal")
-                  maybe-goal]
-                 [else (let ((new-fringe
-                              (for/set ([p (for/fold ([expansions (set)])
-                                             ([p current-fringe])
-                                             (set-union expansions (expand p)))]
-                                        #:unless (or (set-member? prev-fringe p)
-                                                     (set-member? current-fringe p)))
-                                       p)))
-                         (printf "At depth ~a fringe has ~a positions~%" depth (set-count current-fringe))
-                         (fringe-search current-fringe
-                                        new-fringe
-                                        (add1 depth)))]))]))
 
