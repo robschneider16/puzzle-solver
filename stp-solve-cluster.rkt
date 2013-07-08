f8fcaab15bef3fdb802a3b476767b37833a28f9d
Wayne Iba <wayne.iba@gmail.com>
2013-07-08 13:52:01 -0700

    adding merging to remote-expand-fringe (incomplete)

--- stp-solve-cluster.rkt
+++ stp-solve-cluster.rkt
@@ -35,16 +35,56 @@
     (vector-sort! position<? res)
     res))
 
-;; remote-expand-part-fringe: (listof position) -> (setof position)
-;; expand the given positions, ignoring duplicates other than in the new fringe being constructed
-(define (remote-expand-part-fringe list-of-pos)
-  (let ((res (for/vector ([p (for/fold ([expansions (set)])
-                               ([p list-of-pos])
-                               (set-union expansions
-                                          (expand p)))])
-               p)))
+;; a sampling-stat is a (vector int int int (vectorof int)
+;; where the elements are:
+;; 1. total number of positions in the corresponding expansion
+;; 2. minimum hash value of the positions
+;; 3. maximum hash value of the positions
+;; 4. vector of bin-counts for the range min-to-max divided into *n-processors* ranges
+
+;; remote-expand-part-fringe: (listof position) int -> (list sampling-stat (vectorof position))
+;; expand the given positions, ignoring duplicates other than in the new fringe being constructed.
+;; Return a pair containing the stats from this expansion
+(define (remote-expand-part-fringe list-of-pos sample-freq)
+  (let* ((sample-stats (vector 0 +inf.0 -1 empty))
+         (res (for/vector ([p (for/fold ([expansions (set)])
+                                ([p list-of-pos])
+                                (set-union expansions
+                                           (expand p)))])
+                          (vector-set! sample-stats 0 (add1 (vector-ref sample-stats 0)))
+                          (vector-set! sample-stats 1 (min (vector-ref sample-stats 1) (equal-hash-code p)))
+                          (vector-set! sample-stats 2 (max (vector-ref sample-stats 2) (equal-hash-code p)))
+                          (when (zero? (vector-ref sample-stats 0) sample-freq)
+                            (vector-set! sample-stats 3 (cons (equal-hash-code p) (vector-ref sample-stats 3))))
+                          p)))
     (vector-sort! position<? res)
-    res))
+    (list sample-stats res)))
+
+;; remote-expand-fringe: (setof position) (vectorof position) -> (vectorof position)
+;; Distributed version of expand-fringe
+(define (remote-expand-fringe prev current-fringe-vec)
+  ;; distribute expansion work
+  (let* ((samp-freq (/ (vector-length current-fringe-vec) (* 1000 *n-processors*)))
+         (expansions (for/work ([range-pair (make-vector-ranges (vector-length current-fringe-vec))])
+                               ;;(expand-fringe-portion range-pair prev-fringe current-fringe-vec)
+                               (remote-expand-part-fringe (for/list ([i (in-range (first range-pair) (second range-pair))])
+                                                            (vector-ref current-fringe-vec i))
+                                                          samp-freq)
+                               ))
+         (sampling-stats (map first expansions))
+         (just-expansions (map rest expansions)))
+    ;; distribute merging work
+    (apply append ;; GAK! -- do something else here
+           (for/work ([merge-range {make-merge-ranges-from-expansions sampling-stats}])
+                     (merge-expansions merge-range just-expansions)))
+    ))
+
+;; merge-expansions: (list int int) (listof (listof position)) -> (listof position)
+;; given a collection of (partial) fringe expansions and a specification of a range of positions to consider
+;; go through all of them and merge the positions (removing duplicates) in that range into a single collection
+
+;; make-merge-ranges-from-expansions: (listof sampling-stats) -> (list merge-ranges)
+;; decide how to partition the space of positions as distributed over the expansions reflected in the sampling-stats
 
 ;; expand-fringe: (setof position) (setof position) -> (setof position)
 ;; Given a current fringe to expand, and the immediately previous fringe, 
