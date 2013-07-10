5789e3884a2458ef1afb537e7c907e00bbc878ce
Mason <masonnl@yahoo.com>
2013-07-09 19:41:57 -0700

    Made a test for writing to disk

--- riottest/test1.rkt
+++ riottest/test1.rkt
@@ -35,6 +35,37 @@
               0;(- (third (caar res)) (second (caar res))) ; time to re-convert
               (- t3 (vector-ref (car res) 1)) ; time to return
               ))))
+
+;; same as above with writing to disk included
+(define (test-write-run n)
+  (for ([i (in-range n)])
+    (let* ([t0 (current-milliseconds)]
+           [biglist (for/list ([x (in-range (+ i 1000000))]) x)]
+           [t1 (current-milliseconds)]
+           [file-stream (open-output-file "biglist" #:exists 'replace)]
+           [biglist-file (write biglist file-stream)]
+           [ignore (close-output-port file-stream)]
+           [t2 (current-milliseconds)]
+           [res (for/work ([bl (list (length biglist))])
+                          (printf "worker sees ~a at ~a milliseconds ~%" bl (current-milliseconds))
+                          (let* ([ot1 (current-milliseconds)]
+                                 [biglist-file-read (with-input-from-file "biglist" read)] 
+                                 ;[ot2 (current-milliseconds)]
+                                 )
+                            ;(set-mcar! bl ot1)
+                            ;(set-mcdr! bl (current-milliseconds))
+                            (list ot1 (current-milliseconds) (last biglist-file-read))))] 
+           [t3 (current-milliseconds)])
+      (printf "~a ~%" res)
+      (printf "Trial ~a: ~a msec build set, ~a convert to list, ~a send to worker, ~a conv to set, ~a conv back to list, ~a to get the result back~%"
+              i 
+              (- t1 t0); time to build
+              (- t2 t1); time to write to disk
+              (- (car (first res)) t2) ; time to send
+              (-  (cadr (first res)) (car (first res))) ; time to read 
+              0;(- (third (caar res)) (second (caar res))) ; time to re-convert
+              (- t3 (cadr (first res))) ; time to return
+              )))) 
     
 
 #|
@@ -44,6 +75,6 @@
 
 (module+ main
 	 (connect-to-riot-server! "localhost")
-	 (displayln (test-run 5))
+	 (displayln (test-write-run 5))
 	 (displayln "Complete"))
 
