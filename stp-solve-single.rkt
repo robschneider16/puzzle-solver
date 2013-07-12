6530886953fb88629ee95af21ce70428f7d62a2c
Wayne Iba <wayne.iba@gmail.com>
2013-07-11 17:39:21 -0700

    write-to-disk testing

--- stp-solve-single.rkt
+++ stp-solve-single.rkt
@@ -24,21 +24,22 @@
 ;; fringe-search: [file(listof position)] [file(listof position)] int -> ...
 ;; perform a fringe BFS starting at the given state until depth is 0
 (define (fringe-search depth)
-  (let ([prev-fringe (read-from-disk "prev-fringe")]
-        [current-fringe (read-from-disk "current-fringe")]) ; current fringe is a FILE
+  (let ([prev-fringe (list->set (read-from-disk "prev-fringe"))]
+        [current-fringe (list->set (read-from-disk "current-fringe"))]) ; current fringe is a FILE
+    ;;(printf "current-fringe: ~a~%" current-fringe)
     (cond [(or (empty? current-fringe) (> depth *max-depth*)) #f]
           [else
            (let ([maybe-goal (goal-in-fringe? current-fringe)])
              (cond [maybe-goal
                     (print "found goal")
                     maybe-goal]
-                   [else (let* ([new-fringe (for/list ([p (for/fold ([expansions (list)])
-                                                            ([p current-fringe])
-                                                            (cons expansions p))]
-                                                       #:unless (or (set-member? prev-fringe p)
-                                                                    (set-member? current-fringe p)))
-                                              p)])
-                           (printf "At depth ~s fringe has ~a positions~%" depth (count current-fringe))
+                   [else (let* ([new-fringe (for/set ([p (for/fold ([expansions (set)])
+                                                           ([p current-fringe])
+                                                           (set-union expansions (expand p)))]
+                                                      #:unless (or (set-member? prev-fringe p)
+                                                                   (set-member? current-fringe p)))
+                                                     p)])
+                           (printf "At depth ~s fringe has ~a positions~%" depth (set-count current-fringe))
                            
                            #|(when (member depth '(5 6))
                            (for ([s (sort (for/list ([p current-fringe])
@@ -47,13 +48,14 @@
                              (printf "~a~%" s)))|#
                            
                            (rename-file-or-directory "current-fringe" "prev-fringe" #t)
-                           (write-to-disk new-fringe "current-fringe")
+                           (write-to-disk (set->list new-fringe) "current-fringe")
                            (fringe-search (add1 depth)))]))])))
 
 
 
 (block10-init)
 ;(climb12-init)
+(compile-ms-array! *piece-types* *bh* *bw*)
 
 (write-to-disk empty "prev-fringe")
 (write-to-disk (list *start*) "current-fringe")
