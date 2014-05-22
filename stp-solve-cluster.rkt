b926510ba0f0643ed6b89d9b971b5f16396c972a
Wayne Iba <iba@westmont.edu>
2014-05-22 13:19:15 -0700

    Configure Runtime Environment
    added configenv*.rkt files to stpconfigs for the cluster and localhost.
    You should add a symbolic link to whichever one you're using and name the link "configenv.rkt"
    which will then get required by stp-solve-cluster.rkt.
    
    For the time being, commented the commandline means for switching between configurations.

--- stp-solve-cluster.rkt
+++ stp-solve-cluster.rkt
@@ -12,6 +12,7 @@
 (require racket/fixnum)
 (require racket/set)
 
+(require "stpconfigs/configenv.rkt")
 (require "stp-init.rkt"
          "stp-solve-base.rkt"
          "stp-fringefilerep.rkt"
@@ -23,18 +24,19 @@
 ;(profiling-enabled #t)
 
 (provide (all-defined-out))
-
 (define *depth-start-time* "the time from current-seconds at the start of a given depth")
+#|
 (define *master-name* "the name of the host where the master process is running")
 (define *local-store* "the root portion of path to where workers can store temporary fringe files")
 (define *share-store* "the main folder where fringe files are stored (and possibly shared via NFS)")
-;#|
+|#
+#|
 (set! *master-name* "localhost")
 (set! *local-store* "/space/fringefiles/")
 (set! *share-store* "./fringefiles/")
 ;(set! *local-store* "/state/partition1/fringefiles/")
 (define *n-processors* 4)
-;|#
+|#
 #|
 (set! *master-name* "wcp")
 (set! *local-store* "/state/partition1/fringefiles/")
---
+++ stpconfigs/configenvlocal.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(provide (all-defined-out))
+
+
+#| Run Environment Configuration for LOCALHOST
+
+Provide defining values for the following global variables:
+  *master-name*  : typically either "localhost" or "wcp"
+  *local-store*  : where to write partial expansions, e.g., "/state/partition1/fringefiles/" on cluster
+  *share-store*  : where to find current, prev, and proto fringes and where to write segments, e.g., "/share/data2/fringefiles/" on cluster
+  *n-processors* : the number of processors to use
+
+Create a link named "configenv.rkt" to this file if on local machine
+
+|#
+
+(define *master-name* "localhost")
+(define *local-store* "fringefiles/")
+(define *share-store* "fringefiles/")
+(define *n-processors* 4)
+
