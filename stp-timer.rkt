a5000c3bf45fbb6ed36c8f2cc2d0b5192126c880
wfi <iba@westmont.edu>
2014-09-27 23:39:11 -0700

    converted stp-timer.rkt to typed/racket

--- stp-timer.rkt
+++ stp-timer.rkt
@@ -1,8 +1,8 @@
-#lang racket/base
+#lang typed/racket
 
 (provide (all-defined-out))
 
-(struct timer (count total started on) #:mutable #:transparent)
+(struct: timer ([count : Integer] [total : Real] [started : Real] [on : Boolean]) #:mutable #:transparent)
 ;; a timer is a structure: (timer c t s o) where c t and s are numbers and o is a boolean
 ;; count is the number of times the timer has been turned on
 ;; total is the accumulation of time while it has been on, recorded in milliseconds
@@ -10,9 +10,11 @@
 ;; on is boolean for on (#t) or off (#f)
 
 ; create an initialized timer object
+(: make-timer (-> timer))
 (define (make-timer) (timer 0 0 0 #f))
 
 ; start this timer
+(: start-timer (-> timer Void))
 (define (start-timer t) 
   (when (timer-on t) (error "start-timer: attempt to start a timer that is already on"))
   (set-timer-on! t #t)
@@ -20,12 +22,14 @@
   (set-timer-started! t (current-inexact-milliseconds)))
 
 ; stop this timer, accumulating time in the total
+(: stop-timer (-> timer Void))
 (define (stop-timer t)
   (unless (timer-on t) (error "stop-timer: attempt to stop a timer that is already off"))
   (set-timer-total! t (+ (timer-total t) (- (current-inexact-milliseconds) (timer-started t))))
   (set-timer-on! t #f))
   
 ; reset this timer
+(: reset-timer (-> timer Void))
 (define (reset-timer t)
   (set-timer-count! t 0)
   (set-timer-total! t 0)
@@ -33,8 +37,7 @@
 
 ; wraps a call in a timer
 (define-syntax-rule (time-this t e)
-  (let ([result #f])
-    (start-timer t)
-    (set! result e)
-    (stop-timer t)
+  (local [(define stat (start-timer t))
+          (define result e)
+          (define stot(stop-timer t))]
     result))
